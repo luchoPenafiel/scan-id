c2f8e96ae59f271c2053242948c15a1edbba5132
Luciano Peñafiel <luciano.penafiel@aerolab.co>
2022-02-10 18:10:11 -0300

    feat(components): add header

---
+++ src/components/Header/Header.styles.tsx
@@ -0,0 +1,13 @@
+// Vendors
+import styled from 'styled-components';
+
+export const Wrapper = styled.header`
+  padding: 1rem 1.25rem;
+
+  border: 1px solid var(--borderGray);
+
+  h1 {
+    color: var(--blue);
+    font-size: 1.25rem;
+  }
+`;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+// Vendors
+import React from 'react';
+
+// Styles
+import { Wrapper } from './Header.styles';
+
+export const Header = () => {
+  return (
+    <Wrapper>
+      <h1>BankClient</h1>
+    </Wrapper>
+  );
+};
