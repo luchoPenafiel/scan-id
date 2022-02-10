46133d6524b2988213a1da5a8735a14db8d75cdf
Luciano Peñafiel <luciano.penafiel@aerolab.co>
2022-02-10 18:09:59 -0300

    feat(components): add id placeholder

---
+++ src/components/IDPlaceholder/IDPlaceholder.styles.tsx
@@ -0,0 +1,13 @@
+// Vendors
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 160px;
+  width: 260px;
+
+  padding: 1rem;
+
+  border-radius: 12px;
+  background-color: var(--white);
+  box-shadow: 0 10px 20px -6px rgba(0, 0, 0, 0.15);
+`;
---
+++ src/components/IDPlaceholder/IDPlaceholder.tsx
@@ -0,0 +1,17 @@
+// Styes
+import { Wrapper } from './IDPlaceholder.styles';
+
+export const IDPlaceholder = () => (
+  <Wrapper>
+    <svg width={217} height={124} xmlns="http://www.w3.org/2000/svg">
+      <g fill="#F3F2F4" fillRule="evenodd">
+        <path d="M69 0a8 8 0 0 1 8 8v75a8 8 0 0 1-8 8H8a8 8 0 0 1-8-8V8a8 8 0 0 1 8-8h61ZM38.5 68C25.876 68 14.71 69.41 5 72.23V81a5 5 0 0 0 5 5h57a5 5 0 0 0 5-5v-8.77C62.29 69.41 51.124 68 38.5 68Zm0-59C22.075 9 17 21.021 17 34s8.521 29 21.5 29S60 46.979 60 34 54.925 9 38.5 9Z" />
+        <rect x={92} width={125} height={10} rx={5} />
+        <rect x={92} y={114} width={101} height={10} rx={5} />
+        <rect y={114} width={77} height={10} rx={5} />
+        <rect x={92} y={24} width={43} height={10} rx={5} />
+        <rect x={92} y={58} width={77} height={10} rx={5} />
+      </g>
+    </svg>
+  </Wrapper>
+);
