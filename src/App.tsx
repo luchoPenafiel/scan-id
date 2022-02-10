cb929cd852ec8cf1362c8dde1eb03798d5895859
Luciano Peñafiel <luciano.penafiel@aerolab.co>
2022-02-10 18:10:45 -0300

    chore: clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
+// Vendors
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// Components
+import { Header, IDPlaceholder, Wrapper } from './components';
+
+// Styles
+import { GlobalStyles } from './GlobalStyes';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyles />
+      <Header />
+      <Wrapper>
+        <h2>Scan your ID</h2>
+        <IDPlaceholder />
+      </Wrapper>
+    </>
   );
-}
+};
 
 export default App;
