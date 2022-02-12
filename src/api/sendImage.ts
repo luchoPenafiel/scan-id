e1031a4e92bfb684c01446a35f1ce080681629e8
Luciano Peñafiel <lucianopenafiel@gmail.com>
2022-02-11 21:10:04 -0300

    refactor: api call

--- src/api/sendImage.ts
+++ src/api/sendImage.ts
@@ -1,25 +1,27 @@
+const API_URL = 'https://front-exercise.z1.digital';
+
 type APIResponseType = {
   summary: { outcome: string };
 };
 
 export const sendImage = async (image: string) => {
-  return new Promise<APIResponseType>((resolve, reject) => {
-    fetch('https://front-exercise.z1.digital/evaluations', {
-      method: 'POST',
-      body: JSON.stringify({ image: image }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((response: Response) => {
-        if (response.status === 200) {
-          response.json().then((data) => {
-            resolve(data);
-          });
-        }
-      })
-      .catch((error) => {
-        reject(error);
+  return new Promise<APIResponseType>(async (resolve, reject) => {
+    try {
+      const response = await fetch(`${API_URL}/evaluations`, {
+        method: 'POST',
+        body: JSON.stringify({ image: image }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
       });
+
+      if (response.status === 200) {
+        response.json().then((data) => {
+          resolve(data);
+        });
+      }
+    } catch (error) {
+      reject(error);
+    }
   });
 };
